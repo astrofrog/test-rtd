74133dfc965106c4548e3ee0504effa3faabb36b
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-10-25 10:07:23 +0100

    Copy over archive files

--- docs/conf.py
+++ docs/conf.py
@@ -87,6 +87,8 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_extra_path = ['archive']
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
